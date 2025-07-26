e63ab5a33da5866078ddee99c56878ad9722207a
Marco Catarino <marco.catarino60@gmail.com>
2025-07-25 22:32:12 -0500

    delete: we delete a double folder

--- PaySecure/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PaySecure;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
